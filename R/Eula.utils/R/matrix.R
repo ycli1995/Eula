3e620dd23978bd02eb9148e04f2f9f3a5bc1d903
ycli1995 <ycli1995@outlook.com>
2025-12-12 17:50:16 +0800

    Add docs and tests for `rowMeanPct`

--- R/Eula.utils/R/matrix.R
+++ R/Eula.utils/R/matrix.R
@@ -49,51 +49,100 @@ asMatrix.data.frame <- function(object, ...) {
   as.matrix(object)
 }
 
-#' @export
-normMinMax <- function(x) {
-  (x - min(x)) / (max(x) - min(x))
-}
-
-#' @export
-cutMinMax <- function(x, limits = NULL) {
-  if (length(limits) != 2) {
-    fastWarning("'limits' should be a 2-element numeric vector.")
-    return(x)
-  }
-  x[x > limits[2]] <- limits[2]
-  x[x < limits[1]] <- limits[1]
-  x
-}
-
+#' Calculate the row means of logged values
+#'
+#' Calculate the row means of logged values in non-log space.
+#'
+#' @param object An object
+#' @param ... `r .dot_param`
+#'
+#' @returns
+#' A numeric vector containing the row means of `object`. If `object` is a 1-D
+#' array, return a numeric scalar.
+#'
+#' @name rowExpMean
 #' @export rowExpMean
 rowExpMean <- function(object, ...) {
   UseMethod("rowExpMean", object)
 }
 
+#' @param log Logical, whether to transform the result into log-space. Default
+#' is `FALSE`.
+#'
+#' @rdname rowExpMean
+#' @export
+#' @method rowExpMean numeric
+rowExpMean.numeric <- function(object, log = FALSE, ...) {
+  out <- mean(expm1(object))
+  if (log) {
+    return(log1p(out))
+  }
+  out
+}
+
 #' @importFrom Matrix rowMeans
 #' @importClassesFrom Matrix CsparseMatrix
+#'
+#' @rdname rowExpMean
 #' @export
 #' @method rowExpMean CsparseMatrix
 rowExpMean.CsparseMatrix <- function(object, log = FALSE, ...) {
+  out <- Matrix::rowMeans(expm1(object), ...)
   if (log) {
-    return(log1p(Matrix::rowMeans(expm1(object), ...)))
+    return(log1p(out))
   }
-  Matrix::rowMeans(expm1(object), ...)
+  out
 }
 
+#' @rdname rowExpMean
 #' @export
 #' @method rowExpMean matrix
 rowExpMean.matrix <- function(object, log = FALSE, ...) {
   rowExpMean.CsparseMatrix(object = object, log = log, ...)
 }
 
+#' Calculate the means and percents of values
+#'
+#' Calculate the means (in log-spcae) and positive (typically non-zero) percents
+#' for each row in a matrix, where columns are grouping by a factor vector.
+#'
+#' @param object A matrix-like object
+#' @param ... `r .dot_param`
+#'
+#' @name rowMeanPct
 #' @export rowMeanPct
 rowMeanPct <- function(object, ...) {
   UseMethod("rowMeanPct", object)
 }
 
+#' @param cell.groups One of the following:
+#' \itemize{
+#' \item A factor vector containing group labels for columns of `object`. The
+#' length must be equal to `ncol(object)`.
+#' \item A list containing the column indices (integer or character if `object`
+#' has column names) for each group.
+#' }
+#'
+#' @param mean.fxn A function used to calculate the row means of `object`.
+#' Default is \code{\link{rowExpMean}} in non-logged space.
+#' @param min.exp A numeric scalar indicating the minimum value to calculate the
+#' positive percents. Default is 0.
+#'
+#' @returns
+#' A list containing three matrices where each row stands for a feature and each
+#' column stands for a group:
+#' \itemize{
+#' \item `n.cells`: The numbers of positive observations for each feature within
+#' corresponding groups.
+#' \item `avg.exp`: The mean values calculated from `mean.fxn`.
+#' \item `avg.pct`: The percents of positive observations within corresponding
+#' groups.
+#' }
+#'
 #' @importFrom Matrix rowSums
 #' @importClassesFrom Matrix CsparseMatrix
+#'
+#' @rdname rowMeanPct
 #' @export
 #' @method rowMeanPct CsparseMatrix
 rowMeanPct.CsparseMatrix <- function(
@@ -103,6 +152,18 @@ rowMeanPct.CsparseMatrix <- function(
     min.exp = 0,
     ...
 ) {
+  if (is.character(cell.groups)) {
+    cell.groups <- as.factor(cell.groups)
+  }
+  if (is.factor(cell.groups)) {
+    if (!length(cell.groups) == ncol(object)) {
+      stop(
+        "`cell.groups` is a factor whose length (", length(cell.groups), ") is",
+        " not equal to column number (", ncol(object), ") of `object`."
+      )
+    }
+    cell.groups <- split(seq_len(ncol(object)), f = cell.groups)
+  }
   if (!is.list(cell.groups)) {
     stop("'cell.groups' must be a list containing cells for each group.")
   }
@@ -123,9 +184,10 @@ rowMeanPct.CsparseMatrix <- function(
     avg.pct[, i] <- round(n.cells[, i] / length(cells), digits = 3)
     avg.exp[, i] <- mean.fxn(object[, cells, drop = FALSE])
   }
-  list(n.cells = n.cells, avg.pct = avg.pct, avg.exp = avg.exp)
+  list(n.cells = n.cells, avg.exp = avg.exp, avg.pct = avg.pct)
 }
 
+#' @rdname rowMeanPct
 #' @export
 #' @method rowMeanPct matrix
 rowMeanPct.matrix <- function(
@@ -142,4 +204,20 @@ rowMeanPct.matrix <- function(
     min.exp = min.exp,
     ...
   )
+}
+
+#' @export
+normMinMax <- function(x) {
+  (x - min(x)) / (max(x) - min(x))
+}
+
+#' @export
+cutMinMax <- function(x, limits = NULL) {
+  if (length(limits) != 2) {
+    fastWarning("'limits' should be a 2-element numeric vector.")
+    return(x)
+  }
+  x[x > limits[2]] <- limits[2]
+  x[x < limits[1]] <- limits[1]
+  x
 }
--- R/Eula.utils/tests/testthat/test-matrix.R
+++ R/Eula.utils/tests/testthat/test-matrix.R
@@ -6,3 +6,30 @@ test_that("asMatrix", {
 
   expect_equal(asMatrix(mat, chunk.size = 10, verbose = FALSE), as.matrix(mat))
 })
+
+test_that("rowExpMean", {
+  mat <- Matrix::rsparsematrix(20, 10, density = 0.5)
+
+  x1 <- rowExpMean(mat)
+  x2 <- rowExpMean(as.matrix(mat))
+  expect_length(x1, nrow(mat))
+  expect_equal(x1, x2)
+
+  x1 <- rowExpMean(mat, log = TRUE)
+  expect_equal(expm1(x1), x2)
+})
+
+test_that("rowMeanPct", {
+  mat <- Matrix::rsparsematrix(10, 20, density = 0.5)
+  cell.groups <- sample(LETTERS[1:3], ncol(mat), replace = TRUE)
+  out <- rowMeanPct(mat, cell.groups)
+
+  out.names <- c("n.cells", "avg.exp", "avg.pct")
+  expect_true(is.list(out))
+  expect_length(out, 3)
+  expect_named(out, out.names)
+  for (i in out.names) {
+    expect_true(is.matrix(out[[i]]))
+    expect_equal(dim(out[[i]]), c(nrow(mat), length(unique(cell.groups))))
+  }
+})
