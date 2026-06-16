8d79d2059d6a5b41e662511ce6bc8aa57a02e593
ycli1995 <ycli1995@outlook.com>
2026-06-16 11:24:11 +0800

    Forgot to export `ReportMe::Refer`

--- perl/lib/ReportMe.pm
+++ perl/lib/ReportMe.pm
@@ -5,12 +5,14 @@ use warnings;
 
 use ReportMe::HTML;
 use ReportMe::HELP;
+use ReportMe::Refer;
 
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = (
 	@ReportMe::HTML::EXPORT,
 	@ReportMe::HELP::EXPORT,
+	@ReportMe::Refer::EXPORT,
 );
 
 1;
