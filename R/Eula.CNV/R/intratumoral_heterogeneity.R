14ad571a36ae39f786c1d1d34e75f3a2554facae
ycli1995 <ycli1995@outlook.com>
2026-06-30 16:58:42 +0800

    Add `ITHScore`

--- R/Eula.CNV/R/intratumoral_heterogeneity.R
+++ R/Eula.CNV/R/intratumoral_heterogeneity.R
@@ -1,17 +1,32 @@
-
+#' @export 
 ITHScore <- function(cell.embeddings, ...) {
-
-
+  keep <- .filter_cells_for_ith(cell.embeddings)
+  cell.embeddings <- cell.embeddings[keep, , drop = FALSE]
 
   mu <- Matrix::rowMeans(cell.embeddings)
   sigma <- MatrixGenerics::rowSds(cell.embeddings)
 
+  score.per.cell <- Matrix::rowSums((cell.embeddings - mu)^2)
+  score.per.cell <- sqrt(score.per.cell)
+  mean(score.per.cell)
 }
 
-.filter_cells_for_ith <- function(cell.embeddings) {
+.filter_cells_for_ith <- function(cell.embeddings, ...) {
   filtered <- list()
   for (i in seq_len(3)) {
-    
+    filtered[[i]] <- .filter_outliers(cell.embeddings[, i], ...)
   }
+  filtered <- Reduce("&", filtered)
   filtered
+}
+
+.filter_outliers <- function(x, sd = 3, na.rm = TRUE, ...) {
+  mu <- mean(x, na.rm = na.rm)
+  sigma <- sd(x, na.rm = na.rm)
+
+  keep <- x >= (mu - sd * sigma) &
+    x <= (mu + 3 * sigma)
+
+  keep[is.na(x)] <- FALSE
+  keep
 }
